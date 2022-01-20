572526b3e1db49e639901f60e3feb33746e20180
Galinka <galinka2002niko@gmail.com>
2022-01-20 20:03:39 +0300

    LR

---
+++ DuckD/DuckD/MallardDuck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckD
+{
+    public class MallardDuck: Duck
+    {
+        public override string DisPlay()
+        {
+            return "DisPlay" + " " + this.GetType();
+        }
+    }
+}
---
+++ DuckD/DuckD/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallardDuck = new MallardDuck();
+            Duck redheadDuck = new RedheadDuck();
+
+            Duck[] duck = new Duck[] { mallardDuck, redheadDuck };
+            for (int i=0; i<duck.Length; i++)
+            {
+                Console.WriteLine(duck[i].Quack());
+                Console.WriteLine(duck[i].Swim());
+                Console.WriteLine(duck[i].DisPlay());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
